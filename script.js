3651bf17ab0833b776bb709c104de7b1fc2bbf1a
Jasir Nawar <nawar7235@gmail.com>
2022-04-22 21:21:46 -0400

    index.html - Added buttons to html. script.js - Added operation functions. style.css - Added grid

---
+++ script.js
@@ -0,0 +1,16 @@
+// Intialize operations object with operations add, subtract, multiply, divide 
+
+const operations = {
+	add: (num1, num2) => num1 + num2,
+	subtract: (num1, num2) => num1 - num2,
+	multiply: (num1, num2) => num1 * num2,
+	divide: (num1, num2) => num1 / num2
+};
+
+// Operate function calls operations	
+
+function operate(num1, num2, operator) {
+	operations.operate(num1, num2);
+}
+
+
